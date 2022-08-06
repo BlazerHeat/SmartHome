132b03e8240aaa49dc91ee23d257cf6d25d6a199
Vatsal Ghoghari <vghoghari82@gmail.com>
2022-08-06 23:36:20 +0530

    typo

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const URL = process.env.PRODUCTION ? 'http://localhost:8000' : 'https://smart-home-io.herokuapp.com'
+
 const axiosClinet = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: URL,
     withCredentials: true,
 });
 

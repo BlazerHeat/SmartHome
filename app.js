7bd60fbbb3b0837095c1d8b820c05cacad43e549
Vatsal Ghoghari <vghoghari82@gmail.com>
2022-08-07 21:26:21 +0530

    fixed typo

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
     cors({
-        origin: 'http://localhost:3000',
+        origin: '*',
         methods: ['GET', 'POST'],
         credentials: true,
     })
